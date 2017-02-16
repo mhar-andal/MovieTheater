edf248ccd5f2ff4629466430e7681cdbab76973c
Mhar Andal <mharandal96@gmail.com>
2017-02-16 10:57:00 -0600

    Fix method name for seat capacity

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -1,4 +1,5 @@
 class Cinema < ApplicationRecord
   has_many :showings
   has_many :movies, through: :showings
+  has_many :seats
 end
--- spec/models/cinema_and_seat_spec.rb
+++ spec/models/cinema_and_seat_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 describe Cinema do
-  before :each do
-    cinema = Cinema.create(seating_capacity: 30)
-    cinema.seating_capacity.times { Seat.create(cinema: cinema) }
-  end
-
+  cinema = Cinema.create(seating_capacity: 30)
+  cinema.seating_capacity.times { Seat.create(cinema: cinema) }
+  
   describe 'cinema' do
     context 'has a seating capacity' do
-      it { expect(cinema.capacity).to eq 30 }
+      it { expect(cinema.seating_capacity).to eq 30 }
     end
 
     context 'can make multiple seats' do

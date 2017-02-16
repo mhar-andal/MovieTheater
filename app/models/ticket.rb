45e78b244a0818b079d342dfe03605d1137e9a63
Mhar Andal <mharandal96@gmail.com>
2017-02-16 11:38:56 -0600

    Fix error messages

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,6 @@ class Ticket < ApplicationRecord
   validates :email, presence: true
   validates :credit_card, presence: true,
   format: { with: /\A(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})\z/,
-  message: "must be a valid credit card" }
-  validates :expiration_date, presence: true, format: { with: /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/, message: "must be valid expiration date" }
+  message: "must be a valid" }
+  validates :expiration_date, presence: true, format: { with: /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/, message: "must be valid" }
 end

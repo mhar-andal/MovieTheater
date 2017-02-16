3a3a97d565aba0327bdf5b200eb68b2ffe37d270
Mhar Andal <mharandal96@gmail.com>
2017-02-16 11:08:41 -0600

    Add credit card validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,5 +4,10 @@ class Ticket < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :credit_card, presence: true
+  validates :credit_card, format: { with: /^4[0-9]{12}(?:[0-9]{3})?$/, message: "must be a valid credit card" }
+  validates :credit_card, format: { with: /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/, message: "must be a valid credit card" }
+  validates :credit_card, format: { with: /^3[47][0-9]{13}$/, message: "must be a valid credit card" }
+  validates :credit_card, format: { with: /^6(?:011|5[0-9]{2})[0-9]{12}$/, message: "must be a valid credit card" }
+
   validates :expiration_date, presence: true
 end

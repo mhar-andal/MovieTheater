e83526425f8242c568517f18a8d2d49ecdacf0f1
Mhar Andal <mharandal96@gmail.com>
2017-02-16 11:34:37 -0600

    Fix credit card validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,10 +4,10 @@ class Ticket < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :credit_card, presence: true
-  validates :credit_card, format: { with: /^4[0-9]{12}(?:[0-9]{3})?$/, message: "must be a valid credit card" }
-  validates :credit_card, format: { with: /^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$/, message: "must be a valid credit card" }
-  validates :credit_card, format: { with: /^3[47][0-9]{13}$/, message: "must be a valid credit card" }
-  validates :credit_card, format: { with: /^6(?:011|5[0-9]{2})[0-9]{12}$/, message: "must be a valid credit card" }
+  validates :credit_card, format: { with: /\A(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})\z/, message: "must be a valid credit card" }
+  # validates :credit_card, format: { with: /(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}/, message: "must be a valid credit card" }
+  # validates :credit_card, format: { with: /3[47][0-9]{13}/, message: "must be a valid credit card" }
+  # validates :credit_card, format: { with: /6(?:011|5[0-9]{2})[0-9]{12}/, message: "must be a valid credit card" }
 
   validates :expiration_date, presence: true
 end

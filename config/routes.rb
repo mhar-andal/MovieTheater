9f3560cfcd9473de526469c9a6eeaeee5cf37b82
Mhar Andal <email": null>
2017-02-16 17:48:21 -0600

    Add showings new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'cinemas/index'
 
   get 'cinemas/new'
-  get '/showings' => 'showings/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'showings#index'
--- spec/controllers/cinemas_controller_spec.rb
+++ spec/controllers/cinemas_controller_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe CinemasController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
+describe CinemasController do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end

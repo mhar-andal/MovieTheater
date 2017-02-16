0198569dd7185356819b567d700761d4fbc2f98f
Mhar Andal <mharandal96@gmail.com>
2017-02-16 10:31:52 -0600

    Add presence validations to ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,8 @@
 class Ticket < ApplicationRecord
   has_one :reservation
-  validates :full_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :credit_card, presence: true
+  validates :expiration_date, presence: true
 end

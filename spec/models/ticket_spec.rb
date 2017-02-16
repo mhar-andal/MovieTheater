1c9572caced5e6f47a3308d4d7136af4744628fe
Mhar Andal <mharandal96@gmail.com>
2017-02-16 09:40:51 -0600

    Add validation card format for ticket spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -29,23 +29,35 @@ describe Ticket do
     let(:invalid_ticket) { Ticket.create() }
 
     context 'error for no first name' do
-      it { expect(ticket.errors.full_messages).to include "First name can't be blank"}
+      it { expect(invalid_ticket.errors.full_messages).to include "First name can't be blank"}
     end
 
     context 'error for no last name' do
-      it { expect(ticket.errors.full_messages).to include "Last name can't be blank"}
+      it { expect(invalid_ticket.errors.full_messages).to include "Last name can't be blank"}
     end
 
     context 'error for no email' do
-      it { expect(ticket.errors.full_messages).to include "Email can't be blank"}
+      it { expect(invalid_ticket.errors.full_messages).to include "Email can't be blank"}
     end
 
     context 'error for no credit card' do
-      it { expect(ticket.errors.full_messages).to include "Credit card can't be blank"}
+      it { expect(invalid_ticket.errors.full_messages).to include "Credit card can't be blank"}
     end
 
     context 'error for no exp' do
-      it { expect(ticket.errors.full_messages).to include "Expiration date can't be blank"}
+      it { expect(invalid_ticket.errors.full_messages).to include "Expiration date can't be blank"}
+    end
+  end
+
+  describe 'ticket card format' do
+    let(:invalid_order) { Ticket.create(first_name: 'Mhar', last_name: 'Andal', email: 'info@gmail.com', credit_card: '1232151235', expiration_date: '12') }
+
+    context 'creditcard must be 16 characters' do
+      it { expect(invalid_order.errors.full_messages).to include "Credit card has to contain 16 numbers" }
+    end
+
+    context 'expiration date must for formatted as M/YY' do
+      it { expect(invalid_order.errors.full_messages).to include "Expiration date has to be formatted as M/YY"}
     end
   end
 

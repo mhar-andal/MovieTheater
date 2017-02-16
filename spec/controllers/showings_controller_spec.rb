90e8b6dde436da71e72e398fe0d62b63025a86ce
Mhar Andal <email": null>
2017-02-16 17:29:25 -0600

    Add showings controller spec

--- spec/controllers/showings_controller_spec.rb
+++ spec/controllers/showings_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe ShowingsController, type: :controller do
+describe ShowingsController do
+  describe 'GET #index' do
+    it 'responds with status code 200' do
+      get(:index)
+      expect(response).to have_http_status 200
+    end
+  end
 
+  describe 'GET #show' do
+    it 'responds with status code 200' do
+      get(:show)
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'GET #new' do
+    it 'responds with status code 200' do
+      get(:new)
+      expect(response).to have_http_status 200
+    end
+  end
 end

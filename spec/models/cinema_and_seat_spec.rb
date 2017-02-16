f8809ced77c73fce7080c0ccbdb867a18bdb31af
Mhar Andal <mharandal96@gmail.com>
2017-02-16 08:25:57 -0600

    Add cinema and seat spec

---
+++ spec/models/cinema_and_seat_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Cinema do
+  let(:cinema) { Cinema.create(seating_capacity: 30) }
+  cinema.seating_capacity.times { Seat.create(cinema: cinema) }
+
+  describe 'cinema' do
+    context 'has a seating capacity' do
+      it { expect(cinema.capacity).to eq 30 }
+    end
+
+    context 'can make multiple seats' do
+      it { expect(cinema.seats.count).to eq 30  }
+    end
+
+  end
+end

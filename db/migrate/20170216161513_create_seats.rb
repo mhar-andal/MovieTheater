b7da6881044dc9ae8073d8b2e06b3b5c8235468a
Mhar Andal <mharandal96@gmail.com>
2017-02-20 19:48:14 -0600

    Fix typo

--- db/migrate/20170216161513_create_seats.rb
+++ db/migrate/20170216161513_create_seats.rb
@@ -2,7 +2,7 @@ class CreateSeats < ActiveRecord::Migration[5.0]
   def change
     create_table :seats do |t|
       t.integer :cinema_id
-      t.string :seat_indentifier
+      t.string :seat_identifier
       t.timestamps
     end
   end

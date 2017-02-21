81dbad5c3ff4376f2b62d858f45b985834413045
Mhar Andal <email": null>
2017-02-20 21:53:56 -0600

    Work on basic layout - Moving to desktop

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'tickets/new'
+
+  get 'movies/index'
+
+  get 'movies/new'
+
   get 'cinemas/index'
 
   get 'cinemas/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'showings#index'
+  root 'movies#index'
 end

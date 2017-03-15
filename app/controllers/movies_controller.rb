db8e0751729ce3695f150bdd2f8cdf421d8cb896
Mhar Andal <mharandal96@gmail.com>
2017-03-15 15:07:57 -0500

    Attempt to add react plugins

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,35 +2,9 @@ class MoviesController < ApplicationController
   include MoviesHelper
   def index
     @movies = Movie.all
-    respond_to do |format|
-      format.html do
-        render component: 'Movie', props: {
-          movies: JSON.parse(@movies.to_json(:methods => [:showings]))
-          }, tag: 'div'
-      end
-      format.json { render json: @movies.to_json(:include => :showings)}
-    end
   end
 
   def show
     @movie = Movie.find(params[:id])
   end
-
-  private
-
-  def prepareArray(array)
-    ActiveModel::ArraySerializer.new(array, each_serializer: serializer(array))
-  end
-
-  def prepare(resource)
-    serializer(resource).new(resource)
-  end
-
-  def serializer(resource)
-      if resource.respond_to? :name
-        "#{resource.name}Serializer".safe_constantize
-      else
-        "#{resource.class}Serializer".safe_constantize
-      end
-    end
 end
--- app/serializers/movie_serializer.rb
+++
@@ -1,5 +0,0 @@
-class MovieSerializer < ApplicationSerializer
-  attributes :id, :title, :description, :poster_link
-
-  self.root = false
-end
